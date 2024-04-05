6d8c44cca5b6e8eac571d6dd79b51ca90a75195e
Kourtney Reynolds <kourtneyreynolds@Kourtneys-MBP.xmission.com>
2024-04-05 14:33:32 -0600

    Add in the run meds script to play with axios

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,14 @@
 "use strict";
-const squaredArray = (array) => {
-    let squared = new Array(array.length);
-    console.log(squared);
-    let leftIdx = 0;
-    let rightIdx = array.length - 1;
-    for (let i = array.length - 1; i >= 0; i--) {
-        const left = array[leftIdx];
-        const right = array[rightIdx];
-        //if the left is smaller than the right
-        //add the left in that index and move to the left
-        //other wise add the right one and move the right pointer
-        if (Math.abs(left) > Math.abs(right)) {
-            squared[i] = left * left; //put the leftat the end because we know that these two number are the possible largest on either side becasue it is sorted
-            leftIdx++;
-        }
-        else {
-            squared[i] = right * right;
-            rightIdx--;
-        }
-    }
-    return squared;
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-const test = [-3, 1, 2, 3, 5, 6, 8, 9];
-const result = squaredArray(test);
-console.log(result);
-console.log(JSON.stringify(result) == JSON.stringify([1, 4, 9, 9, 25, 36, 64, 81]));
-console.log('cookies');
+Object.defineProperty(exports, "__esModule", { value: true });
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    console.log('This is a cookie here');
+}))();
--- index.ts
+++ index.ts
@@ -1,29 +1,24 @@
-const squaredArray = (array: number[]) => {
 
-    let squared = new Array(array.length)
-    console.log(squared)
-    let leftIdx = 0
-    let rightIdx = array.length - 1
+//Create a basic get and do some stuff with that
+import axios from 'axios'
 
-    for (let i = array.length - 1; i >= 0; i--) {
-        const left = array[leftIdx]
-        const right = array[rightIdx]
-        //if the left is smaller than the right
-        //add the left in that index and move to the left
-        //other wise add the right one and move the right pointer
-        if (Math.abs(left) > Math.abs(right)) {
-            squared[i] = left * left //put the leftat the end because we know that these two number are the possible largest on either side becasue it is sorted
-            leftIdx++
-        } else {
-            squared[i] = right * right
-            rightIdx--
-        }
+type Medicine = {
+    name: string
+    rxnorm: string
+    [key: string] : string | number | undefined
 
-    }
-    return squared;
 }
-    const test = [-3, 1, 2, 3, 5, 6, 8, 9]
-    const result = squaredArray(test)
-    console.log(result)
-    console.log(JSON.stringify(result) == JSON.stringify([1, 4, 9, 9, 25, 36, 64, 81]))
-    console.log('cookies')
+
+const DOMAIN = 'https://rxnav.nlm.nih.gov';
+
+(async () => {
+    console.log('This is a cookie here')
+    const url = new URL(`${DOMAIN}`)
+    const med = 'azithromycin'
+    url.pathname = '/REST/drugs.json'
+    url.search = `?name=${med}`
+    
+
+    const {data} = await axios.request({ url: url.href, headers:{'Content-Type': 'application/json'} })
+    console.log(JSON.stringify(data, null, 2))
+})();
